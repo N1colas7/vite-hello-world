549f6e23fa3d2c161668dac6af2442d24d83b3e6
N1colas7 <nicolasnd9897@gmail.com>
2023-01-10 18:03:55 +0100

    exercise finish

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+        name: "AppTitle",
+        data() {
+            return {
+                hello: 'La mia prima app con Vite!'
+            }
+        },
+        methods:{
+
+        }
+}
+</script>
+<template >
+    <div>
+        {{hello}}
+    </div>
+</template>
+<style>
+    
+</style>
